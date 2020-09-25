52a8a7d978371856a500451ae5577cd086009dd2
Samio Santos <71763791+Samio-Santos@users.noreply.github.com>
2020-09-25 13:48:26 -0300

    Projeto

---
+++ Projeto_Website_Estacionamento-carros/scripts/scripts.js
@@ -0,0 +1,27 @@
+function mudaFoto(foto) {
+    var icon = document.getElementById("parking").src = foto;
+}
+
+
+function isValid_cpf() {
+    var cpf = document.getElementById('cpf')
+    if (cpf.value.length > 11 || cpf.value.length < 11) {
+        alert('CPF inválido! O CPF possui 11 digitos.')  
+        cpf.value = ''
+    }
+    cpf.focus()
+}
+
+// Função que bloqueia teclas especificas no campo "INPUT"
+document.getElementById('cpf').addEventListener('keypress', function(event) {
+    if (event.key == '.' || event.key == '-') {
+        event.preventDefault()
+    }
+    
+});
+
+// Função que permiti que o usuario digite SOMENTE números no campo "input"
+function filtroTeclas(event) {
+    return ((event.charCode >= 48 && event.charCode <= 57) || (event.keyCode == 45 || event.charCode == 46))
+  }
+
